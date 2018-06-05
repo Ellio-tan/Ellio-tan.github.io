d54b4000bc753a9b5a03ce7158e45c014d662dab
Ellio-tan <elliot.kt.tan@gmail.com>
2018-06-05 05:41:13 -0700

    using get instead of getJSN

--- json_jquery_practice/jsonTest.js
+++ json_jquery_practice/jsonTest.js
@@ -1,6 +1,14 @@
 //var url = "http://opendata.toronto.ca/transportation/tmc/rescucameraimages/Data/tmcearthcameras.json"
 
-
- $.getJSON("https://opendata.toronto.ca/transportation/tmc/rescucameraimages/Data/tmcearthcameras.json", function(result){
-        console.log("success")
-    });
+var jqxhr = $.get( "https://opendata.toronto.ca/transportation/tmc/rescucameraimages/Data/tmcearthcameras.json", function() {
+  alert( "success" );
+})
+  .done(function() {
+    alert( "second success" );
+  })
+  .fail(function() {
+    alert( "error" );
+  })
+  .always(function() {
+    alert( "finished" );
+  });
